807b04b6f07bbab46be89869c789ce5bfed7dca1
gabriel-corto <digital.gabriel2006@gmail.com>
2024-11-18 22:07:05 +0100

    feat: add immer to change state data

--- src/reducers/cycles.ts
+++ src/reducers/cycles.ts
@@ -1,39 +1,42 @@
+import { produce } from "immer"
 import { ActionType, CycleState } from "../@types/cycles"
 
 export function cyclesReducer(state: CycleState, action: any){
   switch(action.type) {
+
     case ActionType.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id
-      }
-    case ActionType.INTERRUPT_ACTIVE_CYCLE:
-      const cycleListWithInterruptedOne = state.cycles.map(cycle => {
-        if(cycle.id === state.activeCycleId) {
-          return {...cycle, interruptDate: new Date()}
-        } else {
-          return cycle
-        }
+      return produce(state, draft => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
       })
-      return {
-        ...state, 
-        cycles: cycleListWithInterruptedOne,
-        activeCycleId: null
-      }
+
+    case ActionType.INTERRUPT_ACTIVE_CYCLE:
+      const activeCycleIndex = state.cycles.findIndex(cycle => {
+        return cycle.id === state.activeCycleId
+      })
+
+      if(activeCycleIndex < 0) {
+        return state 
+      } 
+
+      return produce(state, draft => {
+        draft.activeCycleId = null 
+        draft.cycles[activeCycleIndex].interruptDate = new Date()
+      })
+
     case ActionType.MARK_ACTIVE_CYCLE_AS_FINISHED:
-      const cycleListWithFinishedOne = state.cycles.map(cycle => {
-        if(cycle.id === state.activeCycleId) {
-          return {...cycle, finishedDate: new Date()}
-        } else {
-          return cycle
-        }
+      const activeCycleindexToMarkAsFinished = state.cycles.findIndex(cycle => {
+        return cycle.id === state.activeCycleId
+      })
+
+      if(activeCycleindexToMarkAsFinished > 0) {
+        return state 
       }
-      )
-      return {
-        ...state, 
-        cycles: [cycleListWithFinishedOne]
-      }
+
+      return produce(state, draft => {
+        draft.activeCycleId = null
+        draft.cycles[activeCycleindexToMarkAsFinished].finishedDate = new Date()
+      })
     default: 
       return state 
   }
